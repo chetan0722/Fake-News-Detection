b6914afc8ae65e40b2f9ccd6fe4b0bb2b59832b9
CHETAN VERMA <143331873+chetan0722@users.noreply.github.com>
2026-05-19 13:00:58 +0530

    Add files via upload

---
+++ app (1).py
@@ -0,0 +1,26 @@
+
+
+model = pickle.load(open("models/model.pkl","rb"))
+vectorizer = pickle.load(open("models/vectorizer.pkl","rb"))
+
+def clean_text(text):
+    text = text.lower()
+    text = re.sub(r'[^a-zA-Z ]','',text)
+    return text
+
+st.title("Fake News Detection")
+
+news = st.text_area("Enter news text")
+
+if st.button("Predict"):
+
+    news = clean_text(news)
+
+    vec = vectorizer.transform([news])
+
+    pred = model.predict(vec)
+
+    if pred[0] == 1:
+        st.success("Real News")
+    else:
+        st.error("Fake News")
